0373606da9dae8de106ec9ca9077c867a065d0dd
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-11-26 16:56:10 +0600

    add router dom and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
+import App from './App'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFound'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/product',
+		element: <Product />,
+	},
+	{
+		path: '/pricing',
+		element: <Pricing />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '*',
+		element: <PageNotFound />,
+	},
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
