24faeabd0c0684adaf1abec6fd634621bac58ede
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-12-05 08:52:56 +0600

    Updates

---
+++ assets/Logo-ub_65o_u.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-sCey8Piq.js";const s="_logo_7b2ls_1",l={logo:s};function g(){return o.jsx("img",{src:"../logo.png",alt:"WorldWise logo",className:l.logo})}export{g as L};
---
+++ assets/PageNotFound-zRJHfaIz.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-sCey8Piq.js";import{P as r}from"./PageNav-y0-oWqtf.js";import"./Logo-ub_65o_u.js";function s(){return o.jsxs("div",{children:[o.jsx(r,{}),o.jsx("h1",{children:"Page not found 😢"})]})}export{s as default};
